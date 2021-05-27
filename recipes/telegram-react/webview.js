0c117807d154da270f95257842903821b4b32b7a
Nando San Miguel <mythemc@gmail.com>
2021-05-27 12:51:41 -0500

    SOLVED: Telegram react notification badge not showing

--- recipes/telegram-react/webview.js
+++ recipes/telegram-react/webview.js
@@ -5,12 +5,12 @@ const path = require("path");
 module.exports = Franz => {
   const getMessages = function getMessages() {
     let count = 0;
-    const elements = document.querySelectorAll(".dialog-badge:not(.dialog-badge-muted)");
+    const elements = document.querySelectorAll('.dialog-subtitle-badge.badge.badge-24.unread');
     if (elements) {
       for (let i = 0; i < elements.length; i += 1) {
-        if (elements[i].querySelector("span").innerHTML !== 0) {
-          count += parseInt(elements[i].querySelector("span").innerHTML);
-        }
+				if (elements[i].outerText) {
+					count ++;
+				}
       }
     }
     Franz.setBadge(count);
