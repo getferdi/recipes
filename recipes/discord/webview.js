b538eb908c5be941c1ec32f3fb5f733350eb47dc
Herculino Trotta <netotrotta@gmail.com>
2021-07-20 09:24:47 -0300

    Add window styling and a close button

--- recipes/discord/webview.js
+++ recipes/discord/webview.js
@@ -33,7 +33,7 @@ module.exports = Franz => {
 // Polyfill to enable Screen Sharing
 // See https://github.com/electron/electron/issues/16513#issuecomment-602070250
 const { desktopCapturer } = require('electron')
-window.navigator.mediaDevices.getDisplayMedia = () => {
+navigator.mediaDevices.getDisplayMedia = () => {
   return new Promise(async (resolve, reject) => {
     try {
       const sources = await desktopCapturer.getSources({ types: ['screen', 'window'] })
@@ -41,7 +41,87 @@ window.navigator.mediaDevices.getDisplayMedia = () => {
       const selectionElem = document.createElement('div')
       selectionElem.classList = 'desktop-capturer-selection'
       selectionElem.innerHTML = `
+        <style>
+        .close {
+            color: #fff;
+            font: 50px/100% arial, sans-serif;
+            position: absolute;
+            right: 5px;
+            text-decoration: none;
+            text-shadow: 0 1px 0 #fff;
+            top: 5px;
+            padding: 0;
+            border: none;
+            background: none;
+          }
+        .close:after {
+            content: '✖'; /* UTF-8 symbol */
+          }
+        .desktop-capturer-selection {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100vh;
+          background: rgba(30,30,30,.75);
+          color: #fff;
+          z-index: 10000000;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+        .desktop-capturer-selection__scroller {
+          width: 100%;
+          max-height: 100vh;
+          overflow-y: auto;
+        }
+        .desktop-capturer-selection__list {
+          max-width: calc(100% - 100px);
+          margin: 50px;
+          padding: 0;
+          display: flex;
+          flex-wrap: wrap;
+          list-style: none;
+          overflow: hidden;
+          justify-content: center;
+        }
+        .desktop-capturer-selection__item {
+          display: flex;
+          margin: 4px;
+        }
+        .desktop-capturer-selection__btn {
+          display: flex;
+          flex-direction: column;
+          align-items: stretch;
+          width: 145px;
+          margin: 0;
+          border: 0;
+          border-radius: 3px;
+          padding: 4px;
+          background: #252626;
+          text-align: left;
+          transition: background-color .15s, box-shadow .15s;
+        }
+        .desktop-capturer-selection__btn:hover,
+        .desktop-capturer-selection__btn:focus {
+          background: rgba(98,100,167,.8);
+        }
+        .desktop-capturer-selection__thumbnail {
+          width: 100%;
+          height: 81px;
+          object-fit: cover;
+        }
+        .desktop-capturer-selection__name {
+          margin: 6px 0 6px;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          overflow: hidden;
+        }
+        </style>
+
         <div class="desktop-capturer-selection__scroller">
+          <button class="close"></button>
+
           <ul class="desktop-capturer-selection__list">
             ${sources.map(({id, name, thumbnail, display_id, appIcon}) => `
               <li class="desktop-capturer-selection__item">
@@ -51,6 +131,7 @@ window.navigator.mediaDevices.getDisplayMedia = () => {
                 </button>
               </li>
             `).join('')}
+			
           </ul>
         </div>
       `
@@ -65,8 +146,8 @@ window.navigator.mediaDevices.getDisplayMedia = () => {
               if(!source) {
                 throw new Error(`Source with id ${id} does not exist`)
               }
-              
-              const stream = await window.navigator.mediaDevices.getUserMedia({
+
+              const stream = await navigator.mediaDevices.getUserMedia({
                 audio: false,
                 video: {
                   mandatory: {
@@ -84,9 +165,20 @@ window.navigator.mediaDevices.getDisplayMedia = () => {
             }
           })
         })
+
+        document.querySelectorAll('.close')
+        .forEach(button => {
+          button.addEventListener('click', async () => {
+            reject("Closed")
+            selectionElem.remove();
+            console.log("CLOSE");
+            })
+          }
+        )
     } catch (err) {
       console.error('Error displaying desktop capture sources:', err)
       reject(err)
     }
-  })
-}
+	
+  });
+};
