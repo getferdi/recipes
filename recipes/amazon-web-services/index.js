080f16856c0e1b01761a19ae3ca7051fe0e52b00
oda-alexandre <oda-alexandre31@gmail.com>
2021-08-26 13:33:58 +0200

    remove badge

---
+++ recipes/amazon-web-services/index.js
@@ -0,0 +1,2 @@
+module.exports = Ferdi => class amazonwebservices extends Ferdi {
+};
---
+++ recipes/amazon-web-services/webview.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = Ferdi => {
+  const getMessages = function getMessages() {
+    // TODO: Insert your notification-finding code here
+    Ferdi.setBadge(0, 0);
+  };
+
+  Ferdi.loop(getMessages);
+  Ferdi.injectCSS(_path.default.join(__dirname, 'service.css'));
+};
