76a9363de5a905a24a5bbe11dfdd4d94f3aa526e
Vijay A <avijayr@protonmail.com>
2021-08-27 18:08:36 +0530

    removed 'sourceMappingURL' per review comments

--- recipes/github/webview.js
+++ recipes/github/webview.js
@@ -14,4 +14,3 @@ module.exports = Ferdi => {
 
   Ferdi.loop(getMessages);
 };
-// # sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJnaXRodWIvd2Vidmlldy5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
